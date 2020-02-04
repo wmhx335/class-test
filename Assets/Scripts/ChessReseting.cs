60647dfef7527502349c75df8db51bd7c176d2ca
wmhx335 <56949459+wmhx335@users.noreply.github.com>
2020-02-04 20:21:57 +0900

    no message

---
+++ Assets/Scripts/ChessReseting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessReseting 
+{
+
+    public GameManager gameManager;
+
+    public ChessReseting()
+    {
+        gameManager=GameManager.Instance;
+    }
+
+    public struct Chess
+    {
+        //public 
+    }
+
+    /// <summary>
+    /// 棋子位置
+    /// </summary>
+    public struct ChessSteps
+    {
+        public int x,y;
+    }
+
+}
