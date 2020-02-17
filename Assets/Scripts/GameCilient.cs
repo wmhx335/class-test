1c3b2de7a51e84814683cf0595468387f08ce727
wmhx335 <56949459+wmhx335@users.noreply.github.com>
2020-02-17 21:37:26 +0900

    完成客户端和服务器的创建

---
+++ Assets/Scripts/GameCilient.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+/// <summary>
+/// 客户端
+/// </summary>
+public class GameCilient 
+{
+    private string serverAddress;
+    private int port;
+    private TcpClient localClient;
+    private Thread receiveThread;
+
+    public GameCilient(string serverAddress,int port)
+    {
+        this.serverAddress = serverAddress;
+        this.port = port;
+    }
+
+    /// <summary>
+    /// 关闭客户端
+    /// </summary>
+    public void Close()
+    {
+        if (localClient!=null)
+        {
+            localClient.Close();
+        }
+        if (receiveThread!=null)
+        {
+            receiveThread.Abort();
+        }
+    }
+
+    /// <summary>
+    /// 启动客户端的方法
+    /// </summary>
+    /// <returns></returns>
+    public bool Start()
+    {
+        localClient = new TcpClient();
+
+        try
+        {
+            localClient.Connect(IPAddress.Parse(serverAddress),port);
+
+            receiveThread = new Thread(SocketReceiver);
+            receiveThread.Start();
+            Debug.Log("客户端启动");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("客户端连接服务器异常:"+ex.Message);
+            if (!localClient.Connected)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 客户端检测接收到的服务器信息
+    /// </summary>
+    private void SocketReceiver()
+    {
+        if (localClient!=null)
+        {
+            //缓冲区
+            int bufferSize = 8192;
+            byte[] resultBuffer = new byte[bufferSize];
+            do
+            {
+                try
+                {
+                    int byteRead =localClient.Client.Receive(resultBuffer);
+                    if (byteRead==0)
+                    {
+                        Debug.Log("与服务器连接中断");
+                        break;
+                    }
+                    //处理接收到的数据 todo
+
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.Log("客户端接收数据异常："+ex.Message);
+                    Close();
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Assets/Scripts/GameServer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+public class GameServer 
+{
+    //当前服务器监听子线程
+    private Thread connectThread;
+    //当前IP地址
+    private string address;
+    //当前本地端口
+    public int port;
+    //远程客户端
+    private TcpClient romoteClient;
+
+    public GameServer(string address,int port)
+    {
+        this.address = address;
+        this.port = port;
+    }
+
+    /// <summary>
+    /// 实例化服务器端套接字对象
+    /// </summary>
+    private void InitServerSocket()
+    {
+        //缓冲区的大小
+        int bufferSize = 8192;
+        IPAddress ip =IPAddress.Parse(address);
+        TcpListener tcpListener = new TcpListener(ip,port);
+        //监听对象开始监听
+        tcpListener.Start();
+        Debug.Log("服务器已启动");
+        //如果有远程客户端连接，此时就会得到一个对象用于通讯，两边联通
+        romoteClient = tcpListener.AcceptTcpClient();
+        Debug.Log("客户端连接成功");
+        NetworkStream stream=romoteClient.GetStream();
+        //循环接收信息输入
+        do
+        {
+            try
+            {
+                //获取与客户端连接的数据
+                byte[] buffer = new byte[bufferSize];
+                //读取信息
+                int byteRead = stream.Read(buffer,0,bufferSize);
+                //客户端断开连接
+                if (byteRead==0)
+                {
+                    Debug.Log("客户端断开");
+                    break;
+                }
+                //具体处理接受信息的数据
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log("客户端异常："+ex.Message);
+                tcpListener.Stop();
+            }
+        } while (true);
+    }
+
+    /// <summary>
+    /// 启动服务器
+    /// </summary>
+    public void Start()
+    {
+        connectThread = new Thread(InitServerSocket);
+        connectThread.Start();
+
+    }
+
+    /// <summary>
+    /// 关闭服务器
+    /// </summary>
+    public void Close()
+    {
+        //如果客户端连接着服务器，那么需要关闭
+        if (romoteClient!=null)
+        {
+            romoteClient.Client.Shutdown(SocketShutdown.Both);
+        }
+        if (connectThread!=null)
+        {
+            connectThread.Abort();
+        }
+    }
+}
